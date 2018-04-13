eb59b7b3809a70f4ab9850429952dcaecf95671a
Ning Zhang <ni.zhang@sap.com>
2018-04-13 17:37:42 +0800

    add more and delete more code

---
+++ js/elementPosition.js
@@ -0,0 +1,27 @@
+
+var showMyPosition = function getOffset(elem) {
+    if (elem.getBoundingClientRect) {
+        console.log(getOffsetRect(elem));
+
+    } else { // old browser
+        console.log(getOffsetSum(elem));
+
+    }
+}
+
+function getOffsetRect(elem) {
+    var box = elem.getBoundingClientRect()
+    var body = document.body
+    var docElem = document.documentElement
+    var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop
+    var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft
+    var clientTop = docElem.clientTop || body.clientTop || 0
+    var clientLeft = docElem.clientLeft || body.clientLeft || 0
+    var top = box.top + scrollTop - clientTop
+    var left = box.left + scrollLeft - clientLeft
+    return {
+        top: Math.round(top),
+        left: Math.round(left)
+    }
+
+}
